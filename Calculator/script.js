9527654d7f898022eefe7933b299f72411055452
Panu Umbangtalad <panu.umbangtalad@everymatrix.com>
2023-09-05 17:45:54 +0700

    create calculator project

---
+++ Calculator/script.js
@@ -0,0 +1,37 @@
+const numberButton = document.querySelector('.btnNum');
+const operationButton = document.querySelector('.btnOperator');
+const equalButton = document.querySelector('.btnEqu');
+const deleteButton = document.querySelector('.btnDel');
+const clearButton = document.querySelector('.btnClear');
+const display = document.querySelector('.display');
+const firstNum = '';
+const operator = '';
+const secondNum = '';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function percent(a, b) {
+    return b = (b / a) * 100;
+}
+
+function clear() {
+}
+
+function operate(firstNum, operator, secondNum) {
+
+}
+
