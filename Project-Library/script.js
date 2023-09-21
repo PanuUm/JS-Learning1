33c9e289da94168a9f76a1dcf6767a90cabcaac6
Panu Umbangtalad <panu.umbangtalad@everymatrix.com>
2023-09-21 18:26:26 +0700

    add library manual

--- Project-Library/script.js
+++ Project-Library/script.js
@@ -1,25 +1,31 @@
 const showButton = document.getElementById("showDialog");
 const addBook = document.getElementById("addBook");
+let bookTitle = '';
+let bookAuthor = '';
+let bookRead = '';
+let bookshelf = '';
 
 // Show modal
 showButton.addEventListener("click", () => {
     addBook.showModal();
   });
 
-const myLibrary = [];
+const myLibrary = [
+  {bookTitle: 'The Green Mile', author: 'Stephen King', read: 'yes'},
+  {bookTitle: 'Harry Potter', author: 'J. K. Rowling', read: 'no'},
+];
 
-for (let i = 0; i < myLibrary.length; i++) {
-      console.log(myLibrary[newBook]);
-}
+myLibrary.forEach(library => {
+  for (let value in library) {
+    console.log(`${library[value]}`)
+  }
+})
 
-function Book(title, author, read) {
-  this.title = title;
-  this.author = author;
-  this.read = read;
+function Book() {
 }
 
 function addBookToLibrary() {
-  let newBook = new Book(bookTitle.value, bookAuthor.value, bookOptions.value);
+  let newBook = new Book(bookTitle.value, bookAuthor.value, bookRead.value);
   myLibrary.push(newBook);
 
   let bookSubmit = document.querySelector(".submit");
